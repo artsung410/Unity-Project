d03ac1ad64b742035d81a9ae44233a7d99be857e
sungkwonmoon <zpzpskm@naver.com>
2022-07-27 18:33:48 +0900

    rader.sc 모듈화 작업 완료

--- FlakHero/Assets/Scripts/Player/Rader.cs
+++ FlakHero/Assets/Scripts/Player/Rader.cs
@@ -17,6 +17,7 @@ public class Rader : MonoBehaviour
 
     private RotateToMouse rotateToMouse;
 
+
     private void Awake()
     {
         rotateToMouse = GetComponentInParent<RotateToMouse>();
@@ -27,27 +28,35 @@ public class Rader : MonoBehaviour
         {
             EnemyDot = Instantiate(EnemyDotPrefab, GameObject.Find("Canvas").transform);
 
-            float worldPlayerX = player.position.x;
-            float worldPlayerY = player.position.z;
-
-            float wolrdEnemyX = other.transform.position.x;
-            float wolrdEnemyY = other.transform.position.z;
-
-            float deltaX = (wolrdEnemyX - worldPlayerX);
-            float deltaY = (wolrdEnemyY - worldPlayerY);
-
-            Vector3 CanvasPos = new Vector3(RaderCanvas.position.x, RaderCanvas.position.y, 0);
-
-            Vector3 Distance = new Vector3(deltaX, deltaY, 0) * 0.7f;
-
-            Vector3 NewDirection = new Vector3(0, 0, 1f);
-
-            Vector3 direction = Quaternion.AngleAxis(rotateToMouse.eulerAngleY * 1f, NewDirection) * Distance;
-
-
-            EnemyDot.transform.position = CanvasPos + direction;
+            EnemyDot.transform.position = CalculateDirection(other);
 
             Destroy(EnemyDot, 0.02f);
         }
+    }
+
+    Vector3 CalculateDirection(Collider enemy)
+    {
+        // 월드상 플레이어 좌표
+        float worldPlayerX = player.position.x;
+        float worldPlayerY = player.position.z;
+
+        // 월드상 적 좌표 
+        float wolrdEnemyX = enemy.transform.position.x;
+        float wolrdEnemyY = enemy.transform.position.z;
+
+        // 실제 캔버스상에 레이더 좌표를 구한다.
+        Vector3 CanvasVec = new Vector3(RaderCanvas.position.x, RaderCanvas.position.y, 0);
+
+        // 플레이어가 적을 바라보는 방향벡터를 정의한다.
+        Vector3 DeltaVec = new Vector3(wolrdEnemyX - worldPlayerX, wolrdEnemyY - worldPlayerY, 0) * 0.7f;
+
+        // 적 좌표가 회전하는 축
+        Vector3 StandardAxis = new Vector3(0, 0, 1f);
+
+        Vector3 NewDeltaVec = Quaternion.AngleAxis(rotateToMouse.eulerAngleY * 1f, StandardAxis) * DeltaVec;
+
+        Vector3 EnemyDirectionOnRader = CanvasVec + NewDeltaVec;
+
+        return EnemyDirectionOnRader;
     }
 }
